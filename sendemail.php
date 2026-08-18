67e67791eb8b7206b0e46ac7ec48de539b24eace
vaanzstoree-code <vaanzstoree@gmail.com>
2026-08-18 10:27:11 +0700

    Add files via upload

---
+++ sendemail.php
@@ -0,0 +1,31 @@
+<?php
+
+// Define some constants
+define( "RECIPIENT_NAME", "John Doe" );
+define( "RECIPIENT_EMAIL", "youremail@mail.com" );
+
+// Read the form values
+$success = false;
+$userName = isset( $_POST['name'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['name'] ) : "";
+$senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['email'] ) : "";
+$senderPhone = isset( $_POST['phone'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['phone'] ) : "";
+$userSubject = isset( $_POST['subject'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['subject'] ) : "";
+$message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
+
+// If all values exist, send the email
+if ( $userName && $senderEmail && $senderPhone && $userSubject && $message) {
+  $recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
+  $headers = "From: " . $username . " <" . $senderEmail . ">";
+  $msgBody = " Message: " . $message . "";
+  $success = mail( $recipient, $headers, $msgBody );
+
+  //Set Location After Successsfull Submission
+  header('Location: contact.html?message=Successfull');
+}
+
+else{
+	//Set Location After Unsuccesssfull Submission
+  	header('Location: index.html?message=Failed');	
+}
+
+?>
